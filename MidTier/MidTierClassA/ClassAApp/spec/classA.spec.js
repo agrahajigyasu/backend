0db5a8bfa0df6ffd5027db4b7df2543f805e34b2
mayank <mayanksamariya99@gmail.com>
2021-11-10 14:42:54 +0530

    added 8 test in classA spec file

--- MidTier/MidTierClassA/ClassAApp/spec/classA.spec.js
+++ MidTier/MidTierClassA/ClassAApp/spec/classA.spec.js
@@ -1,5 +1,8 @@
 let request= require("request");
 const base_url= "http://localhost:3021/";
+const teamUrl = base_url+ 'classA/bikeservice/team/';
+const toyUrl = base_url + "classA/toyservice/all/";
+const foodUrl =  base_url + "classA/foodservice/all/";
 console.log("Starting test");
 
 
@@ -12,6 +15,65 @@ describe("Class A services are working", () =>{
 				done();
 			});
 		});
+
+	});
+
+	describe("GET /classA/bikeservice/team", () =>{
+        it("returns status code 200", (done) => {
+            request.get(teamUrl, (error, response, body) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        }); 
+
+		it("check result for teams", (done) =>{
+            request.get(teamUrl, (error, response, body) =>{
+                result= JSON.parse(body);
+                expect(result.membersNames.length).toBeGreaterThan(10);
+                expect(result.membersNames[0]).toBe('Aakash Gopal Vachhani');
+                done();
+            });
+        });
+	});
+
+	describe("GET /classA/toyservice/all/", () =>{
+        it("should have more than two elements for India", (done) => {
+            request.get(toyUrl+"India", (error, response, body) =>{
+                result= JSON.parse(body);
+                expect(result.length).toBeGreaterThan(2);
+                expect(result[1].name).toBe('Ferry Boat');
+                done();
+            });
+        });
+
+		it("should have more than two elements for USA", (done) => {
+            request.get(toyUrl+"USA", (error, response, body) =>{
+                result= JSON.parse(body);
+                expect(result.length).toBeGreaterThan(2);
+                expect(result[1].name).toBe('Ferry Boat');
+                done();
+            });
+        });
+	});
+
+	describe("GET /classA/foodservice/all/", () =>{
+        it("should have more than two elements for India", (done) => {
+            request.get(foodUrl+"India", (error, response, body) =>{
+                result= JSON.parse(body);
+                expect(result.length).toBeGreaterThan(2);
+                expect(result[1].name).toBe('Peanut Butter');
+                done();
+            });
+        });
+
+		it("should have more than two elements for USA", (done) => {
+            request.get(foodUrl+"USA", (error, response, body) =>{
+                result= JSON.parse(body);
+                expect(result.length).toBeGreaterThan(2);
+                expect(result[1].name).toBe('Peanut Butter');
+                done();
+            });
+        });
 
 	});
 });
